1dd53911c8522d90db5df0657322507439fd2ce7
ting <zangft@hotmail.com>
2020-07-19 18:15:27 -0400

    1 [CHG] Add Product page

---
+++ SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vic.SportsStore.Domain.Abstract;
+using Vic.SportsStore.Domain.Concrete;
+
+namespace Vic.SportsStore.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IProductsRepository repository { get; set; } 
+            = new InMemoryProductRepository();
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
