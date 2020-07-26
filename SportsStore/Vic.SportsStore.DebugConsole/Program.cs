51aa4e47237ade2f6c4aadae3fc1112964e90a37
ting <zangft@hotmail.com>
2020-07-26 16:04:56 -0400

    1 [ADD] Connect to sqlserver

--- SportsStore/Vic.SportsStore.DebugConsole/Program.cs
+++ SportsStore/Vic.SportsStore.DebugConsole/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vic.SportsStore.Domain.Concrete;
+using Vic.SportsStore.Domain.Entities;
 
 namespace Vic.SportsStore.DebugConsole
 {
@@ -10,6 +12,24 @@ namespace Vic.SportsStore.DebugConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Start.....");
+
+            using(var ctx = new EFDbContext())
+            {
+                var product = new Product()
+                {
+                    Name = "football",
+                    Price = 12.35m,
+                    Category = "ball",
+                    Description = "this is a football"
+                };
+
+                ctx.Products.Add(product);
+                ctx.SaveChanges();
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
         }
     }
 }
